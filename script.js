0ab5755b1404aa0a72e925dfeebdbea4dcacc9d2
Jhon-laboratory <jhonlarico11@gmail.com>
2024-08-30 22:26:17 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+function showCertificate(imageSrc) {
+    var modal = document.getElementById("modal");
+    var modalImage = document.getElementById("modal-image");
+    modal.style.display = "block";
+    modalImage.src = imageSrc;
+}
+
+function closeModal() {
+    var modal = document.getElementById("modal");
+    modal.style.display = "none";
+}
+
+// Cerrar el modal al hacer clic fuera de la imagen
+window.onclick = function(event) {
+    var modal = document.getElementById("modal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
